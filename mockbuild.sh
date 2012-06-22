71ed75f36fc4dc06323a410b4fb4f61d498c64c5
Anselm Strauss <strauss@puzzle.ch>
2012-06-22 13:27:07 +0200

    added mockbuild script

---
+++ mockbuild.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+chroot=$1
+dist=$2
+version=$3
+release=$4
+
+cd `dirname $0`
+
+mock -r $chroot --buildsrpm --spec java-1.6.0-oracle.spec --sources ~/rpmbuild/SOURCES
+cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-$version-puzzle.$release.$dist.src.rpm ~/rpmbuild/SRPMS
+mock -r $chroot --rebuild ~/rpmbuild/SRPMS/java-1.6.0-oracle-$version-puzzle.$release.$dist.src.rpm
+cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-$version-puzzle.$release.$dist.x86_64.rpm ~/rpmbuild/RPMS
+
+mock -r $chroot --buildsrpm --spec java-1.6.0-oracle-devel.spec --sources ~/rpmbuild/SOURCES
+cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-devel-$version-puzzle.$release.$dist.src.rpm ~/rpmbuild/SRPMS
+mock -r $chroot --rebuild ~/rpmbuild/SRPMS/java-1.6.0-oracle-devel-$version-puzzle.$release.$dist.src.rpm
+cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-devel-$version-puzzle.$release.$dist.x86_64.rpm ~/rpmbuild/RPMS

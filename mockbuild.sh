32cde2eb25320ea0531a65e061b9a0aa5d7d15b7
Anselm Strauss <strauss@puzzle.ch>
2012-06-26 12:18:05 +0200

    normal and devel package do not need to have same release number

--- mockbuild.sh
+++ mockbuild.sh
@@ -4,6 +4,7 @@ chroot=$1
 dist=$2
 version=$3
 release=$4
+releaseDevel=$5
 
 cd `dirname $0`
 
@@ -13,6 +14,6 @@ mock -r $chroot --rebuild ~/rpmbuild/SRPMS/java-1.6.0-oracle-$version-puzzle.$re
 cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-$version-puzzle.$release.$dist.x86_64.rpm ~/rpmbuild/RPMS
 
 mock -r $chroot --buildsrpm --spec java-1.6.0-oracle-devel.spec --sources ~/rpmbuild/SOURCES
-cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-devel-$version-puzzle.$release.$dist.src.rpm ~/rpmbuild/SRPMS
-mock -r $chroot --rebuild ~/rpmbuild/SRPMS/java-1.6.0-oracle-devel-$version-puzzle.$release.$dist.src.rpm
-cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-devel-$version-puzzle.$release.$dist.x86_64.rpm ~/rpmbuild/RPMS
+cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-devel-$version-puzzle.$releaseDevel.$dist.src.rpm ~/rpmbuild/SRPMS
+mock -r $chroot --rebuild ~/rpmbuild/SRPMS/java-1.6.0-oracle-devel-$version-puzzle.$releaseDevel.$dist.src.rpm
+cp -v /var/lib/mock/$chroot/result/java-1.6.0-oracle-devel-$version-puzzle.$releaseDevel.$dist.x86_64.rpm ~/rpmbuild/RPMS
